7c50d15f36b64dae27ec802ebaf25fe299c75648
KenZundera <fauzansuken@gmail.com>
2025-02-26 11:58:23 +0700

    add server side for dicoding web

---
+++ dicoding/backend-dicoding-js/3-membangun-web-service/notes-app-back-end/src/server.js
@@ -0,0 +1,24 @@
+// client side url: http://notesapp-v1.dicodingacademy.com/
+
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+    // penerapan cors untuk seluru route
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan di ${server.info.uri}`);
+};
+
+init();
