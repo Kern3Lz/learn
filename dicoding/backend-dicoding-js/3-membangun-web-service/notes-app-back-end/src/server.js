e4d38c759da2057fd66a9770f48c1f0d208982e6
KenZundera <fauzansuken@gmail.com>
2025-03-18 06:34:30 +0700

    minor change

--- dicoding/backend-dicoding-js/3-membangun-web-service/notes-app-back-end/src/server.js
+++ dicoding/backend-dicoding-js/3-membangun-web-service/notes-app-back-end/src/server.js
@@ -1,16 +1,16 @@
 // client side url: http://notesapp-v1.dicodingacademy.com/
 
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: "localhost",
+    host: 'localhost',
     // penerapan cors untuk seluru route
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
